53c310e04ec7c7f69041cbab7ac17aaf355b0078
Maxat Kulmanov <coolmaksat@gmail.com>
2024-06-03 08:39:56 +0300

    variant calling workflow

--- create_input.py
+++ create_input.py
@@ -9,15 +9,15 @@ import gzip
 ck.command()
 def main():
     genomes = {
-        'ksa001.1': '../t2t/data/ksa001.1v1.0.0.fa',
-        'ksa001.2': '../t2t/data/ksa001.2v1.0.0.fa',
+        'ksa001.1': '../t2t/data/ksa001.1/ksa001.1v1.0.0.fa',
+        'ksa001.2': '../t2t/data/ksa001.2/ksa001.2v1.0.0.fa',
     }
     with open('data/input_ksa001.fa', 'w') as w:
         for key, seq_file in genomes.items():
             with open(seq_file, 'r') as f:
                 seqs = SeqIO.parse(f, 'fasta')
                 for rec in seqs:
-                    rec.id = key + '|' + rec.id
+                    rec.id = key + '_' + rec.id
                     SeqIO.write(rec, w, 'fasta')
             
 if __name__ == '__main__':
